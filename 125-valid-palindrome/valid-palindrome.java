9dd13f6b9a4c6e8f7f35d08563c34ae9f4119a68
aadhav01 <01aadhavan@gmail.com>
2026-08-18 11:41:04 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (74.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,35 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String res=s.replaceAll("[^a-zA-Z0-9]", "");
-        res=res.toLowerCase();
-        int left=0;
-        int right=res.length()-1;
-        while(left<right){
-            if(res.charAt(left)!=res.charAt(right)){
+        if(s == null || s.length() == 0) return true;
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left <= right){
+            char a = s.charAt(left);
+            char b = s.charAt(right);
+
+            if(!((a >= 'A' && a <='Z') || ( a >='0' && a <='9') || (a >= 'a' && a <= 'z') )){
+                left++;
+                continue;
+            }
+
+            if(!((b >= 'A' && b <='Z') || ( b >='0' && b <='9') || (b >= 'a' && b <= 'z') )){
+                right--;
+                continue;
+            }
+
+            if((a >= 'A' && a <='Z')) a += 32;
+            if((b >= 'A' && b <='Z')) b += 32;
+
+            if(a != b){
                 return false;
             }
+
             left++;
             right--;
         }
+
         return true;
     }
 }
